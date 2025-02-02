60d237ed50e5f195ed792051385ac7dfc4e229e9
Carl Åstholm <carl@astholm.se>
2025-02-02 14:30:56 +0100

    Update for SDL 3.2.2

--- build.zig
+++ build.zig
@@ -3,9 +3,9 @@
 
 const std = @import("std");
 
-pub const version: std.SemanticVersion = .{ .major = 3, .minor = 2, .patch = 0 };
+pub const version: std.SemanticVersion = .{ .major = 3, .minor = 2, .patch = 2 };
 const formatted_version = std.fmt.comptimePrint("SDL-{}", .{version});
-pub const vendor_info = "https://github.com/castholm/SDL 0.1.3";
+pub const vendor_info = "https://github.com/castholm/SDL 0.1.4";
 pub const revision = formatted_version ++ " (" ++ vendor_info ++ ")";
 
 pub fn build(b: *std.Build) void {
